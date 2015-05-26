503b5cddeb04e9ccb3495c4dfe25786a4398d7af
Misha Wolfson <misha@ginkgobioworks.com>
2015-05-26 17:37:02 -0400

    Add tests for CurousObjects#groupObjectsByID

--- tests/curious.js
+++ tests/curious.js
@@ -59,4 +59,80 @@ describe('CuriousObjects', function () {
       assert.strictEqual(obj.d['4'], 'something');
     });
   });
+
+  describe('#groupObjectsByID', function () {
+    var objs;
+
+    beforeEach(function () {
+      objs = [
+        {name: 'a', id: 1},
+        {name: 'b', id: 2},
+        {name: 'c', id: 3},
+      ];
+    });
+
+    it('should return an object', function () {
+      assert(curious.CuriousObjects.groupObjectsByID(objs) instanceof Object);
+    });
+
+    it('should contain all of the IDs', function () {
+      assert.deepEqual(
+        Object.keys(curious.CuriousObjects.groupObjectsByID(objs)).sort(),
+        [1, 2, 3]
+      );
+    });
+
+    it('should contain all of the objects', function () {
+      assert.deepEqual(
+        curious.CuriousObjects.values(curious.CuriousObjects.groupObjectsByID(objs)).sort(),
+        objs.sort()
+      );
+    });
+
+    it('should match the objects by their IDs', function () {
+      var objsByID = curious.CuriousObjects.groupObjectsByID(objs);
+
+      objs.forEach(function (obj) {
+        assert.strictEqual(obj, objsByID[obj.id]);
+      });
+    });
+
+    it('should maintain only complex references', function () {
+      var objsByID = curious.CuriousObjects.groupObjectsByID(objs);
+
+      // Simple reference
+      objsByID[1] = 'moop'; // objs[0]
+      // Complex reference
+      objsByID[2].name = 'broop'; // objs[1]
+
+      // Simple references do not change
+      assert.strictEqual(JSON.stringify(objs[0]), JSON.stringify({name: 'a', id: 1}));
+      // Complex references do change
+      assert.strictEqual(JSON.stringify(objs[1]), JSON.stringify({name: 'broop', id: 2}));
+
+    });
+
+    it('should take the last object in the arary in case of duplicate IDs', function () {
+      var objsByID;
+
+      objs.push({name: 'b2', id: 2});
+      objsByID = curious.CuriousObjects.groupObjectsByID(objs);
+
+      assert.strictEqual(objsByID[2].name, 'b2');
+    });
+
+    it('should ignore objects without IDs', function () {
+      var objsByID;
+      var objsByIDWithNoID;
+
+      objsByID = curious.CuriousObjects.groupObjectsByID(objs);
+
+      objs.push({name: 'noID'});
+
+      objsByIDWithNoID = curious.CuriousObjects.groupObjectsByID(objs);
+
+      assert.deepEqual(objsByID, objsByIDWithNoID);
+    });
+
+  });
 });
