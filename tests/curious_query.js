b1cbda365abc835004c579ff63976a9e677dd3e4
Misha Wolfson <misha@ginkgobioworks.com>
2015-05-28 18:14:45 -0400

    Add skeleton for CuriousQuery tests

---
+++ tests/curious_query.js
@@ -0,0 +1,9 @@
+// mocha.js tests for the functions dealing with Curious queries
+(function () {
+'use strict';
+
+var expect = require('chai').expect;
+var curious = require('../curious2.js');
+
+
+}());
